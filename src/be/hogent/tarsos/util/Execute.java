05c1a787473f163d9e61ed92db7e783f884a5d3d
Joren Six <joren.six@hogent.be>
2010-03-29 11:17:45 +0200

    Formatting and refactoring. Messages are logged now in stead of printed to stdout.

--- src/be/hogent/tarsos/util/Execute.java
+++ src/be/hogent/tarsos/util/Execute.java
@@ -3,42 +3,58 @@ package be.hogent.tarsos.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Execute {
-	private Execute(){}
-	
-	public static void command(String command, String redirectOutputToFile){	
-		try {			
-			String[] cmd;
-			if(System.getProperty("os.name").contains("indows")){
-				redirectOutputToFile = redirectOutputToFile == null || redirectOutputToFile.equals("") ? "" : " > " + redirectOutputToFile ;
-				cmd = new String[3];
-				cmd[0] = "cmd.exe" ;
-                cmd[1] = "/C" ;
-				cmd[2] =  command + redirectOutputToFile; 
-				System.out.println(command + redirectOutputToFile);
-			}else{
-				redirectOutputToFile = redirectOutputToFile == null || redirectOutputToFile.equals("") ? "" : "| cat > " + redirectOutputToFile ;
-				cmd = new String[3];
-				cmd[0] = "/bin/bash";
-				cmd[1] = "-c";
-				cmd[2] =  command + " 2>&1 " + redirectOutputToFile;
-			}			
-			System.out.println("Execing " + cmd[0] + " " + cmd[1] 
-			                                                   + " " + cmd[2]);
+	private static final Logger log = Logger.getLogger(Execute.class.getName());
 
+	//disable default constructor
+	private Execute(){}
+
+	public static void command(String command, String redirectOutputToFile){
+		try {
+			String[] cmd;
+			if (System.getProperty("os.name").contains("indows")) {
+				cmd = buildWindowsCommand(command, redirectOutputToFile);
+			} else {
+				cmd = buildUinxCommand(command, redirectOutputToFile);
+			}
+			log.info("Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2]);
 			Runtime rt = Runtime.getRuntime();
-            Process proc = rt.exec(cmd);
-            BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line = null;
-            while ( (line = stdout.readLine()) != null)
-                System.out.println(line);
-            int exitVal = proc.waitFor();
-            assert exitVal == 0 : "Process stopped with exit value: " + exitVal;
+			Process proc = rt.exec(cmd);
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(
+					proc.getInputStream()));
+			String line = null;
+			while ((line = stdout.readLine()) != null)
+				System.out.println(line);
+			int exitVal = proc.waitFor();
+			assert exitVal == 0 : "Process stopped with exit value: " + exitVal;
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static String[] buildWindowsCommand(String command, String redirectOutputToFile){
+		redirectOutputToFile = redirectOutputToFile == null
+		|| redirectOutputToFile.equals("") ? "" : " > "
+		+ redirectOutputToFile;
+		String[] cmd = new String[3];
+		cmd[0] = "cmd.exe";
+		cmd[1] = "/C";
+		cmd[2] = command + redirectOutputToFile;
+		return cmd;
+	}
+
+	private static String[] buildUinxCommand(String command, String redirectOutputToFile){
+		redirectOutputToFile = redirectOutputToFile == null
+		|| redirectOutputToFile.equals("") ? "" : "| cat > "
+		+ redirectOutputToFile;
+		String[] cmd = new String[3];
+		cmd[0] = "/bin/bash";
+		cmd[1] = "-c";
+		cmd[2] = command + " 2>&1 " + redirectOutputToFile;
+		return cmd;
 	}
 }
