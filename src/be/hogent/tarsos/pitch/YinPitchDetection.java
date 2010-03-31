57d3712f33177f9aaf5c1eb6dc2064a32e928294
Joren Six <joren.six@hogent.be>
2010-03-31 17:17:46 +0200

    Added a wrapper for the java based YIN implementation.

---
+++ src/be/hogent/tarsos/pitch/YinPitchDetection.java
@@ -0,0 +1,54 @@
+package be.hogent.tarsos.pitch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import be.hogent.tarsos.pitch.Yin.DetectedPitchHandler;
+import be.hogent.tarsos.util.AudioFile;
+
+public class YinPitchDetection implements PitchDetector {
+	private static final Logger log = Logger.getLogger(YinPitchDetection.class.getName());
+
+	private final AudioFile file;
+	private final List<Sample> samples;
+	private final String name;
+
+	public YinPitchDetection(AudioFile audioFile){
+		this.file = audioFile;
+		this.samples = new ArrayList<Sample>();
+		this.name = "java_yin";
+	}
+
+	@Override
+	public void executePitchDetection() {
+		try {
+			Yin.processFile(file.path(), new DetectedPitchHandler() {
+				@Override
+				public void handleDetectedPitch(float time, float pitch) {
+					long start = (long) time * 1000;
+					Sample s = pitch == -1 ? new Sample(start) : new Sample(start,pitch);
+					samples.add(s);
+				}
+			});
+		} catch (UnsupportedAudioFileException e) {
+			log.severe("Unsupported audio file: " + file.basename() + " " + e.getMessage());
+		} catch (IOException e) {
+			log.severe("Exception while reading audio file: " + file.basename() + " " + e.getMessage());
+		}
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public List<Sample> getSamples() {
+		return samples;
+	}
+
+}
