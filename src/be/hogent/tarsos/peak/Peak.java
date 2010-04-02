c9e8f8e912e4ca10f3e71445e27ccc9eeef30f1a
Joren Six <joren.six@hogent.be>
2010-04-02 15:56:08 +0200

    Documentation, formatting

--- src/be/hogent/tarsos/peak/Peak.java
+++ src/be/hogent/tarsos/peak/Peak.java
@@ -1,23 +1,31 @@
 package be.hogent.tarsos.peak;
 
-public class Peak
-{
-	private double position;
-	private double height;
-	
-    /**
-     * Creates a new peak
-     * @param position the position in cents
-     * @param height the height of the peak
-     */
-    public Peak(double position, double height)
-    {
-    	this.position = position;
-        this.height = height;        
-    }
+/**
+ *Defines a Peak in a histogram
+ *
+ * @author Joren Six
+ *
+ */
+public class Peak {
+	private final double position;
+	private final double height;
+
+	/**
+	 * Creates a new peak
+	 *
+	 * @param position
+	 *            the position in cents
+	 * @param height
+	 *            the height of the peak
+	 */
+	public Peak(double position, double height) {
+		this.position = position;
+		this.height = height;
+	}
 
 	/**
 	 * The height of the peak (number of occurrences)
+	 *
 	 * @return the height of the peak
 	 */
 	public double getHeight() {
@@ -26,6 +34,7 @@ public class Peak
 
 	/**
 	 * The position of the peak in cents
+	 *
 	 * @return The position of the peak in cents
 	 */
 	public double getPosition() {
