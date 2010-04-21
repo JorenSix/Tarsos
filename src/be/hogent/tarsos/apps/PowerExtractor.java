ef95e82a585b68976aa80182527b70158984875f
Joren Six <joren.six@hogent.be>
2010-04-21 09:55:13 +0200

    Refactored powerextractor application

---
+++ src/be/hogent/tarsos/apps/PowerExtractor.java
@@ -0,0 +1,24 @@
+package be.hogent.tarsos.apps;
+
+import java.util.List;
+
+import be.hogent.tarsos.util.AudioFile;
+import be.hogent.tarsos.util.SignalPowerExtractor;
+
+public class PowerExtractor {
+	/**
+	 * Extracts power features from some files.
+	 * @param args
+	 */
+	public static void main(String... args){
+		String[] globDirectories = {"makam","maghreb"};
+		List<AudioFile> files = AudioFile.audioFiles(globDirectories);
+		for(AudioFile file:files){
+			System.out.println(file.basename());
+			SignalPowerExtractor spex = new SignalPowerExtractor(file);
+			spex.savePowerPlot("data/tests/power_" + file.basename() + ".png",-40);
+			spex.saveTextFile("data/tests/power_" + file.basename() + ".txt");
+			spex.saveWaveFormPlot("data/tests/waveform_" + file.basename() + ".png");
+		}
+	}
+}
