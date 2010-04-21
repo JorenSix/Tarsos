fc4d31f94b0f655c79742c84a3e5a7e2c1a10278
Joren Six <joren.six@hogent.be>
2010-04-21 17:17:24 +0200

    Changed something to Sample, apperantly.

--- src/be/hogent/tarsos/pitch/Sample.java
+++ src/be/hogent/tarsos/pitch/Sample.java
@@ -21,15 +21,44 @@ public class Sample implements Comparable<Sample> {
 
 	/**
 	 * Defines the unit of the pitch value.
-	 * Absolute Cents are compared to a configured
-	 * reference frequency. By default C0 (16Hz) is used.
 	 *
 	 */
 	public enum PitchUnit{
+		/**
+		 * Oscillations per second.
+		 */
 		HERTZ,
+
+		/**
+		 * Number of cents compared to the "absolute zero"
+		 * a configured, low frequency.
+		 * By default C0 (16Hz) is used.
+		 */
 		ABSOLUTE_CENTS,
+
+		/**
+		 * Number of cents (between 0 and 1200)
+		 * relative to the start of the octave.
+		 * The first octave starts at "absolute zero"
+		 * a configured, low frequency.
+		 */
 		RELATIVE_CENTS,
-		MIDI_KEY;
+
+		/**
+		 * An integer from 0 to 127 that
+		 * represents the closest MIDI key.
+		 * All Hz values above 13289.7300 Hz are mapped to 127,
+		 * all values below a certain value are mapped to 0.
+		 */
+		MIDI_KEY,
+
+		/**
+		 * An double from 0 to 127 that
+		 * represents the closest MIDI key.
+		 * All values above (13289.7300 Hz + 99,99... cents) are mapped to 127,99999...
+		 * All values below a certain value are mapped to 0.00.
+		 */
+		MIDI_CENT;
 	}
 
 	/**
