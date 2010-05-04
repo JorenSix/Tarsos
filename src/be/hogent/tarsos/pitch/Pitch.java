0d4a279df4ef55eaa2c7be7e8daeac51910a275b
Joren Six <joren.six@hogent.be>
2010-05-04 13:48:07 +0200

    Commented pitch methods.

--- src/be/hogent/tarsos/pitch/Pitch.java
+++ src/be/hogent/tarsos/pitch/Pitch.java
@@ -3,8 +3,7 @@ package be.hogent.tarsos.pitch;
 import be.hogent.tarsos.pitch.Sample.PitchUnit;
 
 /**
- * A class representing pitch.
- * Can be used to convert pitch units or to base
+ * A class representing pitch. Can be used to convert pitch units or to base
  * pitch interval calculations on.
  *
  * @author Joren Six
@@ -18,11 +17,11 @@ public class Pitch {
 	private final double pitchInHertz;
 
 	/**
-	 * Create a new pitch object
-	 * with a certain pitch
+	 * Create a new pitch object with a certain pitch
+	 *
 	 * @param pitchInHertz
 	 */
-	private Pitch(double pitchInHertz){
+	private Pitch(double pitchInHertz) {
 		this.pitchInHertz = pitchInHertz;
 	}
 
@@ -30,33 +29,43 @@ public class Pitch {
 	 * Get the pitch value in a given unit.
 	 *
 	 * @param unit
-	 * @return
+	 *            The requested unit.
+	 * @return A pitch value in the requested unit.
 	 */
-	public double getPitch(PitchUnit unit){
+	public double getPitch(PitchUnit unit) {
 		double value = 0.0;
 		switch (unit) {
-			case ABSOLUTE_CENTS:
-				value = PitchConverter.hertzToAbsoluteCent(pitchInHertz);
-				break;
-			case RELATIVE_CENTS:
-				value = PitchConverter.hertzToRelativeCent(pitchInHertz);
-				break;
-			case MIDI_KEY:
-				value = PitchConverter.hertzToMidiKey(pitchInHertz);
-				break;
-			case MIDI_CENT:
-				value = PitchConverter.hertzToMidiCent(pitchInHertz);
-				break;
-			case HERTZ:
-				value = pitchInHertz;
-				break;
-			default:
-				throw new Error("Unsupported unit: " + unit.name());
+		case ABSOLUTE_CENTS:
+			value = PitchConverter.hertzToAbsoluteCent(pitchInHertz);
+			break;
+		case RELATIVE_CENTS:
+			value = PitchConverter.hertzToRelativeCent(pitchInHertz);
+			break;
+		case MIDI_KEY:
+			value = PitchConverter.hertzToMidiKey(pitchInHertz);
+			break;
+		case MIDI_CENT:
+			value = PitchConverter.hertzToMidiCent(pitchInHertz);
+			break;
+		case HERTZ:
+			value = pitchInHertz;
+			break;
+		default:
+			throw new Error("Unsupported unit: " + unit.name());
 		}
 		return value;
 	}
 
-	public int octaveIndex(){
+	/**
+	 * Calculates which octave this pitch falls into. The octave index is based
+	 * on MIDI keys. Keys [0,11] fall in octave -1, [11,23] in octave 0,...
+	 *
+	 * @return The octave the pitch falls into, calculated using MIDI key.
+	 * @exception IllegalArgumentException
+	 *                If the pitch does not fall within the range of valid MIDI
+	 *                KEYS.
+	 */
+	public int octaveIndex() {
 		int midiKey = (int) getPitch(PitchUnit.MIDI_KEY);
 		return (midiKey / 12) - 1;
 	}
@@ -68,8 +77,8 @@ public class Pitch {
 	 * @param hertzValue
 	 *            The pitch in Hz.
 	 * @return A note name like C3, A4 or A3#/B3b.
-	 * @exception IllegalArgumentException When the hertzValue is outside
-	 * the valid MIDI key range.
+	 * @exception IllegalArgumentException
+	 *                When the hertzValue is outside the valid MIDI key range.
 	 */
 	public String noteName() {
 		String name = "";
@@ -82,27 +91,38 @@ public class Pitch {
 		return name;
 	}
 
-
-
-	public static Pitch getInstance(PitchUnit unit,double value){
+	/**
+	 * Return a new pitch object using value in a certain unit.
+	 *
+	 * @param unit
+	 *            The unit of the pitch value.
+	 * @param value
+	 *            The value Itself.
+	 * @return A new Pitch object.
+	 * @exception IllegalArgumentException
+	 *                If RELATIVE_CENTS is given: Cannot convert relative cent
+	 *                value to absolute frequency.
+	 */
+	public static Pitch getInstance(PitchUnit unit, double value) {
 		double hertzValue = Double.MAX_VALUE;
 		switch (unit) {
-			case ABSOLUTE_CENTS:
-				hertzValue = PitchConverter.absoluteCentToHertz(value);
-				break;
-			case RELATIVE_CENTS:
-				throw new IllegalArgumentException("Cannot convert relative cent value to absolute frequency. Pitch object creation failed.");
-			case MIDI_KEY:
-				hertzValue = PitchConverter.midiKeyToHertz((int) value);
-				break;
-			case MIDI_CENT:
-				hertzValue = PitchConverter.midiCentToHertz(value);
-				break;
-			case HERTZ:
-				hertzValue = value;
-				break;
-			default:
-				throw new Error("Unsupported unit: " + unit.name());
+		case ABSOLUTE_CENTS:
+			hertzValue = PitchConverter.absoluteCentToHertz(value);
+			break;
+		case RELATIVE_CENTS:
+			throw new IllegalArgumentException(
+					"Cannot convert relative cent value to absolute frequency. Pitch object creation failed.");
+		case MIDI_KEY:
+			hertzValue = PitchConverter.midiKeyToHertz((int) value);
+			break;
+		case MIDI_CENT:
+			hertzValue = PitchConverter.midiCentToHertz(value);
+			break;
+		case HERTZ:
+			hertzValue = value;
+			break;
+		default:
+			throw new Error("Unsupported unit: " + unit.name());
 		}
 		return new Pitch(hertzValue);
 	}
