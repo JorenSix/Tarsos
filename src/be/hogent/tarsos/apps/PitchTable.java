be2b4d38e36cd763027d58ff611c1fbcb448e315
Joren Six <joren.six@hogent.be>
2010-05-04 13:54:46 +0200

    Import statements

--- src/be/hogent/tarsos/apps/PitchTable.java
+++ src/be/hogent/tarsos/apps/PitchTable.java
@@ -1,8 +1,7 @@
 package be.hogent.tarsos.apps;
 
 import be.hogent.tarsos.pitch.Pitch;
-import be.hogent.tarsos.pitch.PitchConverter;
-import be.hogent.tarsos.pitch.Sample.PitchUnit;
+import be.hogent.tarsos.pitch.PitchUnit;
 
 public class PitchTable {
 
@@ -14,12 +13,12 @@ public class PitchTable {
 		System.out.println("---------------------------------------------------------------------------");
 		for(int i = 0 ; i < 128 ; i++){
 			Pitch p = Pitch.getInstance(PitchUnit.MIDI_KEY, i);
+
 			double frequency = p.getPitch(PitchUnit.HERTZ);
 			double absoluteCents = p.getPitch(PitchUnit.ABSOLUTE_CENTS);
 			double relativeCents = p.getPitch(PitchUnit.RELATIVE_CENTS);
 			int octaveIndex = p.octaveIndex();
-			//sanity check
-			assert i == PitchConverter.hertzToMidiKey(frequency);
+
 			System.out.printf("%4d %10s %14.5f Hz %14.0f  %14.0f %10d\n",i,p.noteName(), frequency,absoluteCents,relativeCents,octaveIndex);
 		}
 	}
