1097d533ea070ff5bd310b2aba96687274e4d497
Joren Six <joren.six@hogent.be>
2010-05-25 11:51:04 +0200

    A (possibly) more precise implementation using System.nanoTime();

--- src/be/hogent/tarsos/util/StopWatch.java
+++ src/be/hogent/tarsos/util/StopWatch.java
@@ -1,25 +1,42 @@
 package be.hogent.tarsos.util;
 
-import java.util.Date;
-
 /**
- * StopWatch counts the ticks passed between initialization and invocation of
- * ticksPassed.
- * 
+ * StopWatch counts the ticks (ns) passed between initialization and invocation
+ * of ticksPassed.
  * @author Joren Six
  */
-public class StopWatch {
-    private final long ticks;
+public final class StopWatch {
 
+    /**
+     * Conversion factor between ns and ms.
+     */
+    private static final int NS_TO_MS = 1000;
+
+    /**
+     * Number of ticks between start and stop in ns (10^-9 s).
+     */
+    private final transient long ticks;
+
+    /**
+     * Create and start the stop watch.
+     */
     public StopWatch() {
-        ticks = new Date().getTime();
+        ticks = System.nanoTime();
     }
 
     /**
      * @return The number of ticks passed between initialization and the call to
-     *         <code>ticksPassed</code>
+     *         <code>ticksPassed</code>. In milliseconds or 10^-3 seconds.
      */
     public long ticksPassed() {
-        return new Date().getTime() - ticks;
+        return Math.abs((System.nanoTime() - ticks) / NS_TO_MS);
+    }
+
+    /**
+     * @return The number of ticks passed between initialization and the call to
+     *         <code>ticksPassed</code>. In nanoseconds or 10^-9 seconds.
+     */
+    public long nanoTicksPassed() {
+        return Math.abs(System.nanoTime() - ticks);
     }
 }
