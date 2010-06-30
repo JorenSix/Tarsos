5ce481760aaefe72fee9266032fa12db32d78a3b
Joren Six <joren.six@hogent.be>
2010-06-30 09:46:44 +0200

    Pitch detection uses AudioDispatcher now.

--- src/be/hogent/tarsos/pitch/pure/TarsosPitchDetection.java
+++ src/be/hogent/tarsos/pitch/pure/TarsosPitchDetection.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -16,11 +15,11 @@ import be.hogent.tarsos.apps.Tarsos;
 import be.hogent.tarsos.pitch.PitchDetectionMode;
 import be.hogent.tarsos.pitch.PitchDetector;
 import be.hogent.tarsos.pitch.Sample;
+import be.hogent.tarsos.sampled.AudioDispatcher;
+import be.hogent.tarsos.sampled.AudioProcessor;
 import be.hogent.tarsos.util.AudioFile;
 import be.hogent.tarsos.util.SimplePlot;
 import be.hogent.tarsos.util.StopWatch;
-
-import com.sun.media.sound.AudioFloatInputStream;
 
 public final class TarsosPitchDetection implements PitchDetector {
 
@@ -91,8 +90,7 @@ public final class TarsosPitchDetection implements PitchDetector {
             final DetectedPitchHandler detectedPitchHandler)
     throws UnsupportedAudioFileException, IOException {
         final AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
-        final AudioFloatInputStream afis = AudioFloatInputStream.getInputStream(ais);
-        processStream(afis, detectedPitchHandler,detectionMode);
+        processStream(ais, detectedPitchHandler, detectionMode);
     }
 
     /**
@@ -107,22 +105,11 @@ public final class TarsosPitchDetection implements PitchDetector {
      * @throws IOException
      *             If there is an error reading the stream.
      */
-    public static void processStream(final AudioFloatInputStream afis,
+    public static void processStream(final AudioInputStream ais,
             final DetectedPitchHandler detectedPitchHandler, final PitchDetectionMode detectionMode)
     throws UnsupportedAudioFileException,
     IOException {
-        final AudioFormat format = afis.getFormat();
-        final float sampleRate = format.getSampleRate();
-        final double frameSize = format.getFrameSize();
-        final double frameRate = format.getFrameRate();
-        float time = 0;
-        // number of bytes / frameSize * frameRate gives the number of seconds
-        // because we use float buffers there is a factor 2: 2 bytes per float?
-        // Seems to be correct but a float uses 4 bytes: confused programmer is
-        // confused.
-        final float timeCalculationDivider = (float) (frameSize * frameRate / 2);
-        long floatsProcessed = 0;
-
+        final float sampleRate = ais.getFormat().getSampleRate();
         final PurePitchDetector pureDetector;
         final int bufferSize;
         final int overlapSize;
@@ -142,69 +129,36 @@ public final class TarsosPitchDetection implements PitchDetector {
         }
 
         final int bufferStepSize = bufferSize - overlapSize;
-        final float[] audioBuffer = new float[bufferSize];
 
-        // read full buffer
-        boolean hasMoreBytes = afis.read(audioBuffer, 0, bufferSize) != -1;
-        floatsProcessed += audioBuffer.length;
-        while (hasMoreBytes) {
-            final float pitch = pureDetector.getPitch(audioBuffer);
-            time = floatsProcessed / timeCalculationDivider;
-            if (detectedPitchHandler != null) {
+
+        final AudioDispatcher dispatcher = new AudioDispatcher(ais, bufferSize, overlapSize);
+        dispatcher.addAudioProcessor(new AudioProcessor() {
+            private long samplesProcessed = 0;
+            private float time = 0;
+
+            @Override
+            public void processFull(final float[] audioFloatBuffer, final byte[] audioByteBuffer) {
+                final float pitch = pureDetector.getPitch(audioFloatBuffer);
+                samplesProcessed += audioFloatBuffer.length;
+                time = samplesProcessed / sampleRate;
                 detectedPitchHandler.handleDetectedPitch(time, pitch);
             }
 
-            // slide buffer with predefined overlap
-            hasMoreBytes = slideBuffer(afis, audioBuffer, overlapSize);
+            @Override
+            public void processOverlapping(final float[] audioFloatBuffer, final byte[] audioByteBuffer) {
+                final float pitch = pureDetector.getPitch(audioFloatBuffer);
+                samplesProcessed += bufferStepSize;
+                time = samplesProcessed / sampleRate;
+                detectedPitchHandler.handleDetectedPitch(time, pitch);
+            }
 
-            floatsProcessed += bufferStepSize;
-        }
-    }
+            @Override
+            public void processingFinished() {
+            }
+        });
 
-    /**
-     * Slides a buffer with an overlap and reads new data from the stream. to
-     * the correct place in the buffer. E.g. with a buffer size of 9 and overlap
-     * of 3.
-     * <pre>
-     *      | 0 | 1 | 3 | 3 | 4  | 5  | 6  | 7  | 8  |
-     *                        |
-     *                Slide (9 - 3 = 6)
-     *                        |
-     *                        v
-     *      | 6 | 7 | 8 | _ | _  | _  | _  | _  | _  |
-     *                        |
-     *        Fill from 3 to (3+6) exclusive
-     *                        |
-     *                        v
-     *      | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 |
-     * </pre>
-     * @param audioInputStream
-     *            The stream to read audio data from.
-     * @param audioBuffer
-     *            The buffer to read audio data to. If consecutive buffers are
-     *            read and an overlap is wanted it should contain the previous
-     *            window.
-     * @param overlap
-     *            The overlap: the number of elements that are copied in the
-     *            buffer from the previous buffer. Overlap should be smaller
-     *            (strict) than the buffer size and can be zero.
-     * @return True if the stream can deliver more data, false otherwise.
-     * @throws IOException
-     *             When something goes wrong while reading the stream. In
-     *             particular, an IOException is thrown if the input stream has
-     *             been closed.
-     */
-    public static boolean slideBuffer(final AudioFloatInputStream audioInputStream,
-            final float[] audioBuffer, final int overlap) throws IOException {
-        assert overlap < audioBuffer.length;
+        new Thread(dispatcher).run();
 
-        final int bufferStepSize = audioBuffer.length - overlap;
-
-        for (int i = 0; i < overlap; i++) {
-            audioBuffer[i] = audioBuffer[i + bufferStepSize];
-        }
-
-        return audioInputStream.read(audioBuffer, overlap, bufferStepSize) != -1;
     }
 
     public static void main(final String... args) throws UnsupportedAudioFileException, IOException {
