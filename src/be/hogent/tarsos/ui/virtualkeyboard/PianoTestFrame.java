2b207283fa1a27671f42d427bed8e73d7189fba7
Joren Six <joren.six@hogent.be>
2010-09-01 16:53:47 +0200

    Refactored virtualkeyboard

--- src/be/hogent/tarsos/ui/virtualkeyboard/PianoTestFrame.java
+++ src/be/hogent/tarsos/ui/virtualkeyboard/PianoTestFrame.java
@@ -2,17 +2,29 @@ package be.hogent.tarsos.ui.virtualkeyboard;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.IOException;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import be.hogent.tarsos.apps.PlayAlong;
+import be.hogent.tarsos.midi.DumpReceiver;
+import be.hogent.tarsos.midi.MidiCommon;
+import be.hogent.tarsos.midi.MidiUtils;
+import be.hogent.tarsos.midi.ReceiverSink;
 
 public class PianoTestFrame extends JFrame {
 
 	private static final long serialVersionUID = 6063312726815482475L;
 
 	public PianoTestFrame(final VirtualKeyboard keyboard, final double[] tuning) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		final Dimension dimension = new Dimension(650, 100);
 		setPreferredSize(dimension);
@@ -24,6 +36,32 @@ public class PianoTestFrame extends JFrame {
 		keyboardPanel.add(keyboard, BorderLayout.CENTER);
 
 		this.add(keyboard, BorderLayout.CENTER);
+
+		final MidiDevice.Info synthInfo = MidiCommon.getMidiDeviceInfo("Gervill", true);
+		MidiDevice synthDevice;
+		try {
+			synthDevice = MidiSystem.getMidiDevice(synthInfo);
+			synthDevice.open();
+
+			Receiver recv;
+			recv = new ReceiverSink(true, synthDevice.getReceiver(), new DumpReceiver(System.out));
+			keyboard.setReceiver(recv);
+
+			final double[] rebasedTuning = PlayAlong.tuningFromPeaks(tuning);
+
+			MidiUtils.sendTunings(recv, 0, 2, "african", rebasedTuning);
+			MidiUtils.sendTuningChange(recv, VirtualKeyboard.CHANNEL, 2);
+		} catch (MidiUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidMidiDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(final String... strings) {
