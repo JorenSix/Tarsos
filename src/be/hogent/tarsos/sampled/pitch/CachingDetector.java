a6a20acd637af0a8f3943345161c4d256f8f4257
JorenSix <joren.six@hogent.be>
2010-10-26 09:58:29 +0200

    Created a decorator for pitch detector: serializes the annotations in a unified, human readable format.

---
+++ src/be/hogent/tarsos/sampled/pitch/CachingDetector.java
@@ -0,0 +1,66 @@
+package be.hogent.tarsos.sampled.pitch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import be.hogent.tarsos.util.AudioFile;
+import be.hogent.tarsos.util.FileUtils;
+
+/**
+ * Caches the results of a pitch detector by serializing annotations to a file.
+ * If a file with annotations exists it reads the contents, otherwise the pitch
+ * detector is executed and the annotations are stored.
+ * 
+ * @author Joren Six
+ */
+public final class CachingDetector implements PitchDetector {
+	private final List<Annotation> annotations;
+	private final AudioFile file;
+	private final PitchDetector detector;
+
+	/**
+	 * Log messages.
+	 */
+	private static final Logger LOG = Logger.getLogger(CachingDetector.class.getName());
+
+	/**
+	 * Create a new caching pitch detector.
+	 * 
+	 * @param audioFile
+	 *            The file to cache results for.
+	 * @param pitchDetector
+	 *            The detector used.
+	 */
+	public CachingDetector(final AudioFile audioFile, final PitchDetector pitchDetector) {
+		file = audioFile;
+		detector = pitchDetector;
+		annotations = new ArrayList<Annotation>();
+	}
+
+	public void executePitchDetection() {
+		String directory = file.transcodedDirectory();
+		String annotationsFileName = detector.getName() + "_" + file.basename() + ".txt";
+		annotationsFileName = FileUtils.combine(directory, annotationsFileName);
+		if (FileUtils.exists(annotationsFileName)) {
+			annotations.addAll(FileUtils.readPitchAnnotations(annotationsFileName));
+			LOG.info(String.format("Read cached annotations for %s from %s", detector.getName(),
+					annotationsFileName));
+		} else {
+			detector.executePitchDetection();
+			annotations.addAll(detector.getAnnotations());
+			FileUtils.writePitchAnnotations(annotationsFileName, annotations);
+			LOG.info(String.format("Cached annotation results for %s to %s", detector.getName(),
+					annotationsFileName));
+		}
+	}
+
+	public List<Annotation> getAnnotations() {
+		return annotations;
+	}
+
+	public String getName() {
+		return "cached_" + detector.getName();
+	}
+
+}
