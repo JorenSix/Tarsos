46ee2a24cdb4ea638bedbcf5e2722f8ac6cdddc6
JorenSix <joren.six@hogent.be>
2011-04-01 10:16:09 +0200

    Created an utility class to model background tasks. It extends SwingWorker and has a user interface with a progress bar.

---
+++ src/be/hogent/tarsos/ui/BackgroundTask.java
@@ -0,0 +1,94 @@
+package be.hogent.tarsos.ui;
+
+import java.awt.GridLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+
+
+public abstract class BackgroundTask extends SwingWorker<Void, Void>   implements PropertyChangeListener{
+	
+	public interface TaskHandler{
+		void taskDone(BackgroundTask backgroundTask);
+		void taskInterrupted(BackgroundTask backgroundTask,Exception e);
+	}
+
+	private final JPanel ui;
+	private final JProgressBar progressBar;
+	private final List<BackgroundTask.TaskHandler> handlers;
+	private final boolean determinedLength;
+	
+
+	protected BackgroundTask(String name, boolean lengthDetermined) {
+		ui = new JPanel(new GridLayout(0,1));
+		handlers = new ArrayList<BackgroundTask.TaskHandler>();
+		progressBar = new JProgressBar();
+		determinedLength = lengthDetermined;
+		
+		ui.add(new JLabel(name));
+		ui.add(progressBar);
+
+		this.addPropertyChangeListener(this);
+		
+		progressBar.setStringPainted(true);
+		if(!lengthDetermined){
+			progressBar.setString("");
+		}
+	}
+	
+	public void addHandler(final BackgroundTask.TaskHandler handler){
+		handlers.add(handler);
+	}
+	
+	public void done(){
+		progressBar.setIndeterminate(false);
+		progressBar.setValue(100);
+		progressBar.setString("Done");
+		for(TaskHandler handler : handlers){
+			handler.taskDone(this);
+		}
+	}
+	
+	/**
+	 * Invoked when task's progress property changes. Executed in event
+	 * dispatch thread
+	 */
+	public void propertyChange(PropertyChangeEvent evt) {
+		if ("progress" == evt.getPropertyName()) {
+			int progress = (Integer) evt.getNewValue();
+			if(!determinedLength){
+				progressBar.setIndeterminate(true);
+				progressBar.setValue(progress);
+				//prevents the progress bar from drawing 
+				//the progress
+				progressBar.setString("");
+			} else {
+				progressBar.setValue(progress);
+			}	
+		}
+	}
+	
+	/*
+	 * Main task. Executed in background thread.
+	 */
+	@Override
+	public abstract Void doInBackground();
+	
+	
+	public void interrupt(BackgroundTask backgroundTask,Exception e){
+		for(TaskHandler handler : handlers){
+			handler.taskInterrupted(this,e);
+		}
+	}
+
+	public JComponent ui() {
+		return ui;
+	}
+}
