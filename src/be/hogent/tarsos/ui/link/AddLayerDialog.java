afc35ad85b30c16f67b0488d160c8722badce90d
ThomasStubbe <stubbe.thomas@gmail.com>
2013-08-23 17:17:51 +0200

    update interface

--- src/be/hogent/tarsos/ui/link/AddLayerDialog.java
+++ src/be/hogent/tarsos/ui/link/AddLayerDialog.java
@@ -34,9 +34,14 @@ public class AddLayerDialog extends JDialog implements ItemListener, ActionListe
 	private final String FEATURE_CQT = "CQT";
 	private final String FEATURE_PITCH = "Pitch contour";
 	
+	private Integer[] frameSizes = {512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+	private Float[] overlapPercent = {0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
+	
 	private Color color;
 	
 	private JComboBox featureTypeList;
+	private JComboBox frameSizeList;
+	private JComboBox overlapList;
 	private JComboBox colorList;
 	
 	private JButton createButton;
@@ -73,43 +78,66 @@ public class AddLayerDialog extends JDialog implements ItemListener, ActionListe
 		
 		String[] featureItems = { this.FEATURE_WAVEFORM, this.FEATURE_CQT, this.FEATURE_PITCH };
 		featureTypeList = new JComboBox(featureItems);
-//		featureTypeList.setEnabled(false);
 		featureTypeList.addItemListener(this);
+		
 		
 		String[] kleuren = { "Grijs", "Rood", "Blauw", "Groen", "Geel"};
 		colorList = new JComboBox(kleuren);
+		
+		frameSizeList = new JComboBox(frameSizes);
+		frameSizeList.addItemListener(this);
+		
+		
+		overlapList = new JComboBox(overlapPercent);
+		overlapList.addItemListener(this);
+		
+		frameSizeList.setSelectedIndex(5);
+		overlapList.setSelectedIndex(5);
+		featureTypeList.setSelectedItem(FEATURE_WAVEFORM);
 		
 //		JLabel lblTypeLayer = new JLabel("Type layer: ");
 		JLabel lblTypeFeature = new JLabel("Type feature: ");
 		JLabel lblColor = new JLabel("Kleur: ");
 		JLabel lblFrameSize = new JLabel("Framesize: ");
 		JLabel lblOverlapping = new JLabel("Overlapping: ");
+
 		createButton = new JButton("Create");
 		createButton.addActionListener(this);
 		cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(this);
 		
-//		contentPanel.add(lblTypeLayer);
 		contentPanel.add(lblTypeFeature);
 		contentPanel.add(featureTypeList);
 		
+		
+		contentPanel.add(lblFrameSize);
+		contentPanel.add(frameSizeList);
+		
+		contentPanel.add(lblOverlapping);
+		contentPanel.add(overlapList);
+		
 		contentPanel.add(createButton);
 		contentPanel.add(cancelButton);
-//		contentPanel.add(lblColor);
-		featureType = this.FEATURE_WAVEFORM;
-		frameSize = 32768;
-		overlap = 0.8f;
-//		this.add(colorList);
-		this.getContentPane().add(contentPanel);
 
-		
-		
+		this.getContentPane().add(contentPanel);
 	}
 
 //	@Override
+	@SuppressWarnings("deprecation")
 	public void itemStateChanged(ItemEvent arg0) {
 		if (arg0.getSource().equals(featureTypeList)) {
 			featureType = arg0.getItemSelectable().getSelectedObjects()[0].toString();
+			if (featureType.equals(this.FEATURE_WAVEFORM)){
+				this.frameSizeList.enable(false);
+				this.overlapList.enable(false);
+			} else {
+				this.frameSizeList.enable(true);
+				this.overlapList.enable(true);
+			}
+		} else if (arg0.getSource().equals(frameSizeList)) {
+			frameSize = (Integer)arg0.getItemSelectable().getSelectedObjects()[0];
+		} else if (arg0.getSource().equals(overlapList)){
+			overlap = (Float)arg0.getItemSelectable().getSelectedObjects()[0];
 		}
 	}
 	
---
+++ src/be/hogent/tarsos/ui/link/layers/segmentationlayers/SegmentationLayer.java
@@ -0,0 +1,58 @@
+package be.hogent.tarsos.ui.link.layers.segmentationlayers;
+
+import java.awt.Graphics2D;
+
+import be.hogent.tarsos.tarsossegmenter.model.AASModel;
+import be.hogent.tarsos.ui.link.LinkedPanel;
+import be.hogent.tarsos.ui.link.layers.Layer;
+
+public class SegmentationLayer  implements Layer{
+
+		
+		protected AASModel model;
+		private int frameSize;
+		private int overlap;
+		private int niveau;
+		
+		protected final LinkedPanel parent;
+
+		public void initialise(){
+			//TODO: AASMODEL: configuratieparameters setten
+		}
+
+		public SegmentationLayer(final LinkedPanel parent, int niveau) {
+			this.parent = parent;
+			frameSize = 0;
+			overlap = 0;
+			this.niveau = niveau;
+		}
+
+		public void run() {
+			AASModel.getInstance().calculate();
+			AASModel.getInstance().getSegmentation().getSegmentationLists(niveau);
+		}
+
+		public int getFrameSize() {
+			return frameSize;
+		}
+
+		public void setFrameSize(int frameSize) {
+			this.frameSize = frameSize;
+		}
+
+		public int getOverlap() {
+			return overlap;
+		}
+
+		public void setOverlap(int overlap) {
+			this.overlap = overlap;
+		}
+
+		public void draw(Graphics2D graphics) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+
+}
